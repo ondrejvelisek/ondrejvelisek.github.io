05eae63cd0827c24b38190910e7a6e2c09f5e54e
Ondřej Velíšek <ondrejvelisek@gmail.com>
2025-03-24 19:39:31 +0100

    update docusaurus and improve config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,55 +1,58 @@
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
-  title: 'Ondrej Velisek',
-  tagline: 'Web app developer',
-  favicon: 'img/favicon.ico',
+  title: "Ondrej Velisek",
+  tagline: "Web app developer",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://ondrejvelisek.github.io',
+  url: "https://ondrejvelisek.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ondrejvelisek', // Usually your GitHub org/user name.
-  projectName: 'ondrejvelisek.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "ondrejvelisek", // Usually your GitHub org/user name.
+  projectName: "ondrejvelisek.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   trailingSlash: true,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: false && {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
         },
         blog: {
-          routeBasePath: '/',
+          routeBasePath: "/",
           showReadingTime: true,
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
         },
         pages: {
-          path: 'pages',
-          exclude: ['**/[A-Z]*.{js,jsx,ts,tsx,md,mdx}'], // Components are with a capital initial letter
+          path: "pages",
+          exclude: ["**/[A-Z]*.{js,jsx,ts,tsx,md,mdx}"], // Components are with a capital initial letter
         },
         theme: {
-          customCss: './global.css',
+          customCss: "./global.css",
         },
         gtag: {
-          trackingID: 'G-BZWT5GPVTE',
+          trackingID: "G-BZWT5GPVTE",
           anonymizeIP: true,
         },
       } satisfies Preset.Options,
@@ -72,17 +75,17 @@ const config: Config = {
 
   themeConfig: {
     colorMode: {
-      defaultMode: 'dark',
+      defaultMode: "dark",
       disableSwitch: true,
       respectPrefersColorScheme: false,
     },
     // Replace with your project's social card
     //image: 'img/torrta-logo.png',
     navbar: {
-      style: 'dark',
+      style: "dark",
       logo: {
-        alt: 'ondrejvelisek Logo',
-        src: 'img/ondrejvelisek-logo.svg',
+        alt: "ondrejvelisek Logo",
+        src: "img/ondrejvelisek-logo.svg",
       },
       items: [
         /*{
@@ -104,28 +107,35 @@ const config: Config = {
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `<div class="flex justify-between">
         <div>Copyright © ${new Date().getFullYear()} Ondrej Velisek</div>
         <div>Made with ❤️ in Czech republic.</div>
       </div>`,
     },
     prism: {
+      theme: {
+        ...prismThemes.vsDark,
+        plain: {
+          ...prismThemes.vsDark.plain,
+          backgroundColor: "#212432",
+        },
+      },
       magicComments: [
         {
-          className: 'theme-code-block-highlighted-line',
-          line: 'highlight-next-line',
-          block: {start: 'highlight-start', end: 'highlight-end'},
+          className: "theme-code-block-highlighted-line",
+          line: "highlight-next-line",
+          block: { start: "highlight-start", end: "highlight-end" },
         },
         {
-          className: 'code-block-error-line',
-          line: 'error-next-line',
-          block: {start: 'error-start', end: 'error-end'},
+          className: "code-block-error-line",
+          line: "error-next-line",
+          block: { start: "error-start", end: "error-end" },
         },
         {
-          className: 'code-block-error-msg',
-          line: 'error-msg-next-line',
-          block: {start: 'error-msg-start', end: 'error-msg-end'},
+          className: "code-block-error-msg",
+          line: "error-msg-next-line",
+          block: { start: "error-msg-start", end: "error-msg-end" },
         },
       ],
     },
